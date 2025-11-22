f20ee8069b505697c6cb14a9176b7e96a78ddf44
davidmatei1902 <davidmatei@gmail.com>
2025-11-22 16:43:19 +0200

    Project Init

---
+++ src/main/java/io/github/davidmatei1902/uni_assets/UniAssetsApplication.java
@@ -0,0 +1,13 @@
+package io.github.davidmatei1902.uni_assets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniAssetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/davidmatei1902/uni_assets/UniAssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davidmatei1902.uni_assets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniAssetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
